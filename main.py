2d21a9ed3310dd084c67ee2098f394fbd88a01a2
JasonOnes <jasonr.jones14@gmail.com>
2017-07-12 10:23:44 -0700

    links to body page done

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask,request, redirect, render_template
+from flask import Flask,request, redirect, render_template, url_for
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
@@ -32,40 +32,62 @@ def main_page():
     return render_template('welcome.html', blogs=blogs)
     
 @app.route('/new_blog', methods=['GET', 'POST'])
-def blog():
-    if request.method == 'POST':
-        #blog = Blog.query.get(blog_id)
-        title = request.form['blog-title']
-        body = request.form['body']
-        new_blog = Blog(title, body)
-        db.session.add(new_blog)
-        db.session.commit()
-        blogs = Blog.query.all()
-
-        return render_template('/welcome.html', blogs=blogs)
+def bloggit():
+#     if request.method == 'POST':
+#         #blog = Blog.query.get(blog_id)
+#         title = request.form['blog-title']
+#         body = request.form['body']
+#         new_blog = Blog(title, body)
+#         db.session.add(new_blog)
+#         db.session.commit()
+#         blogs = Blog.query.all()
+# #put error flashes here?
+#         if not title:
+#             # flash error message
+#             pass
+#         elif not body:
+#             # flash error message 2
+#             pass
+#         return redirect('/')#, blogs=blogs)
     
-    return render_template('new_blog.html')#, blogs=blogs)
+    return render_template('/new_blog.html')#, blogs=blogs)
 
 # @app.route('/blog_pass', methods=['POST'])
 # def pass_off():
 #     blogid = Blog.query(id)
 #     return redirect('/blog/?blogid='+ blogid)
 
-@app.route('/blog', methods=['GET','POST'])
+@app.route('/blog', methods=['POST'])
 def see_body():
-    # # if request.method == 'POST':
-    # # blog_num = int(request.form['blog-id'])
-    # blog = Blog.query.get(blog.id)
-    # #     return redirect('/blog', blog=blog)
-    # #blog_id = int(request.form['blog-id'])
-    # blog_title = request.form['blog-title']
-    # blogx = Blog.query.get(blog_title)
-    # #blogx = Blog.query.filter_by(blog.title).all()
+   
+    title = request.form['blog-title']
+    body = request.form['body']
+    new_blog = Blog(title, body)
+    db.session.add(new_blog)
+    db.session.commit()
+    print(new_blog.id)
+    return render_template('/blog.html', blog=new_blog)
 
-    if request.method == 'GET':
-        return render_template('/blog.html', blog=blog)
-
-
+@app.route('/blog?<blog_id>', methods=['GET', 'POST'])
+   
+def blog_page(blog_id):
+    #blogid = Blog.query.get(id)
+    #print(blogid)
+    print(blog_id)
+    # if blogid == None:
+    #     #error
+    #     pass
+    # elif blog_id == blogid:
+    check_blog = Blog.query.filter_by(id=blog_id).first()
+    return render_template('/blog.html', blog=check_blog)
+    """blogid = request.args.get(id)
+    blog = Blog.query.filter_by(id=blogid).first()"""
+    #blog_title = request.args.get('blog-title')
+    # blog_body = request.args.get('body')
+    #blog_x = Blog.query.filter_by(title=blog_title).first() 
+    #blog_id = request.args.get('id')
+    #return render_template(url_for('see_body', id=blog.id), blog=blog)
+    #return render_template('/blog.html', blog=blog)
 
 if __name__ == ('__main__'):
     app.run()
