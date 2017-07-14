736da68e2886aafee05900135a9861de270060dc
JasonOnes <jasonr.jones14@gmail.com>
2017-07-14 07:59:15 -0700

    blog url dynamic, moved sort from jinja to main

--- main.py
+++ main.py
@@ -23,14 +23,18 @@ class Blog(db.Model):
     def __repr__(self):
         return 'The {} blog contains {}.'.format(self.title, self.body)
 
-@app.route('/')#, methods=['GET', 'POST'])
+@app.route('/')
 def main_page():
-    # if request.method == 'POST':
-    #     return render_template('welcome.html')
-    blogs = Blog.query.all()
+    #TODO get users preference for how they want blogs sorted on main page
+    blog_sort = request.args.get('blog_sort')
+    if blog_sort == 'newest':
+        blogs = Blog.query.order_by(Blog.entry_date.desc()).all()
+    # sort alphabetically, by author, length of body?
+    else:
+        blogs = Blog.query.all()
     return render_template('welcome.html', blogs=blogs)
     
-@app.route('/new_blog')#, methods=['GET','POST'])
+@app.route('/new_blog')
 def bloggit():    
     return render_template('/new_blog.html')
 
@@ -38,30 +42,29 @@ def bloggit():
 # Once the blog has been written it commits to databas after correct view is rendered thus letting us 
 # edit if we'd like and then reference by created id
 def see_body():
-    title = request.form['blogtitle']
-    body = request.form['body']
-    if not title:
-        flash("You need to title your post!", "error")
-        print(body)
-        return render_template('/new_blog.html', body=body)
-    if not body:
-        flash("You haven't actually blogged about anything!", "error")
-        print(title)
-        return render_template('/new_blog.html', title=title)
-    new_blog = Blog(title, body, entry_date=datetime.now()) #utcnow())
-    db.session.add(new_blog)
-    db.session.commit()
-    print(new_blog.id)
-    return render_template('/blog.html', blog=new_blog)
-    # blogid = str(new_blog.id)
-    # return redirect('/blog?id={}'.format(blogid))
+    if request.method == 'POST':
+        title = request.form['blogtitle']
+        body = request.form['body']
+        if not title and not body:
+            flash("You haven't entered ANYTHING!", "error")
+            return render_template('/new_blog.html')
+        if not title:
+            flash("You need to title your post!", "error")
+            return render_template('/new_blog.html', body=body)
+        if not body:
+            flash("You haven't actually blogged about anything!", "error")
+            return render_template('/new_blog.html', title=title)
+        new_blog = Blog(title, body, entry_date=datetime.now()) #utcnow())
+        db.session.add(new_blog)
+        db.session.commit()
+        return render_template('/blog.html', blog=new_blog)
 
-@app.route('/blog?id=<blog_id>')
+@app.route('/blog/<blog_id>/')
 #  gets the blogs id from the query paramater and then passes that blog to template
-def blog_page(blog_id):
+def see_blog_page(blog_id):
     check_blog = Blog.query.filter_by(id=blog_id).first()
     return render_template('/blog.html', blog=check_blog)
 
+
 if __name__ == ('__main__'):
     app.run()
-
